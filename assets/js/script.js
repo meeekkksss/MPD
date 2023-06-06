c5bc2b82eaf053f5c6982596e15f4d2c6f01fa92
pakuhn <pakuhn93@gmail.com>
2023-06-06 15:53:45 -0700

    trying to pull the right data from new API

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,12 @@ var animalAPI = "https://api.gbif.org/v1/species/"; //the end should add an inte
 var keyAnimals = "epzENF8Vuzg2XiYxDER5/g==LHqnoNfdprsV6lsX";
 var plantsAPI = "https://perenual.com/api/species-list?page=1&key=";
 var keyPlants = "sk-kDQd647e4a0a7cc661162";
-var animalName = 'frog';
 var plantsPageRange = 377; //last page of the plants API that we are using
+var animalsRange = 8000;
 
 var myPlant = '';
-var myAnimal = '';
+var myAnimal = 'pig';
 
-
-
-animalAPI = "https://api.api-ninjas.com/v1/animals?name=" + animalName;
 plantsAPI = "https://perenual.com/api/species-list?page=1&key=" + keyPlants;
 
 //gives a random number between 0 and num, not including num
@@ -18,15 +15,8 @@ function randNum(num){
     return Math.floor(Math.random() * num);
 }
 
-
-//gets an animal from an api
 function getAnimal(num){
-    fetch(animalAPI, {
-        method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/animals?name=cheetah',
-        headers: { 'X-Api-Key': keyAnimals},
-        contentType: 'application/json',
-        })
+    fetch(animalAPI + num)
         .then(function (response){
             return response.json();
         })
@@ -44,9 +34,9 @@ function getAnimal(num){
 
 //gets a plant from an api
 function getPlant(num){
-    var plantsPage = plantsAPI.replace("page=1", "page=" + num)
-    console.log("My Page: " + plantsPage);
-    fetch(plantsPage)
+    var plantPage = plantsAPI.replace("page=1", "page=" + num)
+    console.log("My Page: " + plantPage);
+    fetch(plantPage)
     .then(function (response){
         return response.json();
     })
@@ -62,7 +52,8 @@ function getPlant(num){
 }
 
 function generateIngredients(){
-    getPlant(randNum(plantsPageRange));
+    // getPlant(randNum(plantsPageRange));
+    getAnimal(randNum(animalsRange));
 }
 
 generateIngredients();  
