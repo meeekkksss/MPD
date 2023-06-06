b2d9a1630d1404dc50a511d38ceb28c718685d77
Merika <merikatoby143@gmail.com>
2023-06-06 10:35:56 -0700

    Burgers + JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,3 +75,29 @@ function tempFunc() {
 
 tempFunc();
 
+
+// // temp function 
+// const axios = require("axios");
+
+// const options = {
+//   method: "GET",
+//   url: "https://plants2.p.rapidapi.com/api/plants",
+//   params: {
+//     id: "63ef4eb7476230641c4c0d62",
+//   },
+//   headers: {
+//     Authorization:
+//       "GKZOHNZj0xP65kk0BAE2Tl9LGagm0pfD3DFNxAEEZcMQBhRZVDco8vbNJdnwwCo0",
+//     "X-RapidAPI-Key": "98093986c1mshbfa8f9e3eccd4c9p12077ejsncf3d0f12ce42",
+//     "X-RapidAPI-Host": "plants2.p.rapidapi.com",
+//   },
+// };
+
+// try {
+//   const response = await axios.request(options);
+//   console.log(response.data);
+// } catch (error) {
+//   console.error(error);
+// }
+
+
