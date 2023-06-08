023147356dfd31814439da6bc2de2e9a7f63c127
pakuhn <pakuhn93@gmail.com>
2023-06-08 13:52:30 -0700

    starting up the button timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,27 +73,23 @@ function init() {
 
 init();
 
-var submitBtn = document.getElementById("submit-btn");
-var resubmitBtn = document.getElementById("resubmit-btn");
+var submitBtn = document.getElementById('submit-btn');
+var resubmitBtn = document.getElementById('resubmit-btn');
 
 //generates the sandwich string to be placed onto the page
-function generateSandwich() {
-  //need to pull 1 random animal and 1 random plant from each database
-  //this code will replicate that process but will likely be placed elsewhere
-  submitBtn.setAttribute;
-  var sandwichMsg = `Bon appettit! We call this one ${myAnimal} con ${myPlant} sandwich!\nEnjoy your scrumptuous sandwich!`;
-  outputBox.textContent = sandwichMsg;
-  init();
-  setTimeout(testFunc, 10000);
+function generateSandwich(){   
+    //need to pull 1 random animal and 1 random plant from each database
+    //this code will replicate that process but will likely be placed elsewhere
+    submitBtn.setAttribute
+    var sandwichMsg = `Bon appettit! We call this one ${myAnimal} con ${myPlant} sandwich!\nEnjoy your scrumptuous sandwich!`;
+    outputBox.textContent = sandwichMsg;
+    init();
+    setTimeout(testFunc, 10000);
 }
 
 submitBtn.addEventListener('click', generateSandwich);
 
-function testFunc() {
+function testFunc(){
   console.log(`Testing`);
 }
 
-submitBtn.addEventListener("click", function () {
-  localStorage.setItem("myPlant", myPlant);
-  localStorage.setItem("myAnimal", myAnimal);
-});
